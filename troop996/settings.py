6fabc9a0e352310931a233b3971e4951fda707c5
slag223 <48025172+slag223@users.noreply.github.com>
2021-01-05 08:52:30 -0500

    restart

--- troop996/settings.py
+++ troop996/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_i2oze!pg&6hx$=qf1lq)0tjrwq@)lc=ycvk-7tbg0-^)p5#7+'
+SECRET_KEY = 'sw@!p$da2b^t1g)#j6&vzz1&rp$c1v#d6h=#$#phoe#@era$4o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
 ]
 
 MIDDLEWARE = [
