e5923057bce4cc23609d211f7ecc3ff121054cba
slag223 <48025172+slag223@users.noreply.github.com>
2021-01-06 16:42:52 -0500

    added model

--- main/views.py
+++ main/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Post
 # Create your views here.
 
-emails = [
-    {
-        'author': 'Davis Plude',
-        'date': '1/7/2021',
-        'title': 'New Campout!!!',
-        'content': 'There will be a new campout next month'
-    }
-]
+
 @login_required
 def main(request):
     context = {
-        'emails': emails
+        'posts': Post.objects.all()
     }
     return render(request, 'main/main.html', context)
 
