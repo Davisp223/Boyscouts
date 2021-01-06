9cfbccf4a24fc4e3b3f3b7a621828fed9e7db7d6
slag223 <48025172+slag223@users.noreply.github.com>
2021-01-06 10:34:22 -0500

    a bunch of stuff

--- home/views.py
+++ home/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+news = [
+    {
+        'date': '1/6/2021',
+        'title': 'New Campouts',
+        'body': 'There will be a new campout next month'
+    }
+]
+
 def home(request):
-    return render(request, 'home/home.html')
+    context = {
+        'news': news
+    }
+    return render(request, 'home/home.html', context)
 
 def recap(request):
-    return HttpResponse('<h1>Meeting Recap Page')
-
+    return render(request, 'home/recap.html')
--- troop996/settings.py
+++ troop996/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'troop996.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'troop996',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
