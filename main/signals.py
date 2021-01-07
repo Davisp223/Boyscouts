c1eebdd92f7e9e07d92127992f6a31a20aed3bd0
slag223 <48025172+slag223@users.noreply.github.com>
2021-01-07 13:23:20 -0500

    work you idot

--- main/signals.py
+++ main/signals.py
@@ -1,21 +1,19 @@
-from django.dispatch import receiver
-from django.db.models import signals
-from .models import Post
+import imghdr
 import os
 import smtplib
-import imghdr
 from email.message import EmailMessage
 
+from django.db.models import signals
+from django.dispatch import receiver
 
+from .models import Post
 
 
 @receiver(signals.post_save, sender=Post)
 def send_mail(sender, instance, created, **kwargs):
-
-    context = {
-        'posts': Post.objects.all()
-        }
-
+    breakpoint()  #from Python3.7
+    model = Post
+    context_object_name = 'posts'
     # to test if the signal is working
     print('signal send')
     # replace it with your code once it works
@@ -28,15 +26,7 @@ def send_mail(sender, instance, created, **kwargs):
     msg['Subject'] = 'Troop996 Automessage -- Do Not Reply'
     msg['From'] = EMAIL_ADDRESS 
     msg['To'] = file = open("main/contacts.txt", "r")
-
-    msg.add_alternative("""\
-    <!DOCTYPE html>
-    <html>
-     <body>
-           <h1 style="color:SlateGray;"> {{ content }}</h1>
-        </body>
-    </html>
-    """, subtype='html')
+    msg.set_content = f"Lets see if this works {content}"
 
 
 
