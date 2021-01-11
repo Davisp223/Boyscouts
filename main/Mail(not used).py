960715e3f2895233382426bfcda548e87a577712
slag223 <48025172+slag223@users.noreply.github.com>
2021-01-11 18:44:56 -0500

    commited out the contacts

--- main/Mail(not used).py
+++ main/Mail(not used).py
@@ -1,4 +1,3 @@
-import os
 import smtplib
 import imghdr
 from email.message import EmailMessage
@@ -11,10 +10,10 @@ contacts = ['Email_ADDRESS', 'davisp713@gmail.com',]
 msg = EmailMessage()
 msg['Subject'] = 'Troop996 Automessage -- Do Not Reply'
 msg['From'] = EMAIL_ADDRESS 
-msg['To'] = file = open("mycontacts.txt", "r")
+msg['To'] = file = open("contacts.txt", "r")
 
 
-msg.add_alternative("""\
+msg.add_alternative("""
 <!DOCTYPE html>
 <html>
     <body>
@@ -28,4 +27,4 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
 
-#print(msg)
+print(msg)
--- main/signals.py
+++ main/signals.py
@@ -2,33 +2,35 @@ import imghdr
 import os
 import smtplib
 from email.message import EmailMessage
-
 from django.db.models import signals
 from django.dispatch import receiver
-
 from .models import Post
 
 
 @receiver(signals.post_save, sender=Post)
 def send_mail(sender, instance, created, **kwargs):
-    breakpoint()  #from Python3.7
-    model = Post
-    context_object_name = 'posts'
     # to test if the signal is working
     print('signal send')
     # replace it with your code once it works
     EMAIL_ADDRESS = "troop996automessager@gmail.com"
     EMAIL_PASSWORD = "Troop996admin"
 
-    contacts = ['Email_ADDRESS', 'davisp713@gmail.com',]
 
     msg = EmailMessage()
     msg['Subject'] = 'Troop996 Automessage -- Do Not Reply'
     msg['From'] = EMAIL_ADDRESS 
-    msg['To'] = file = open("main/contacts.txt", "r")
-    msg.set_content = f"Lets see if this works {content}"
+    #msg['To'] = file = open("main/contacts.txt", "r")
 
 
+    msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+    <body>
+        <h1 style="color:SlateGray;">Hello, I hope your day is going well. We will be having our weekely meeting tonight on zoom at 7:00pm, the code is https://us04web.zoom.us/j/2507889269?pwd=emZRbk5Lb2kxT2ZKQytJN1hpQmVBdz09</h1>
+        <p>Message sent Via Troop996automessager.  If you need to respond to this message in any way please contact davisp713@gmail.com</p>
+    </body>
+    </html>
+    """, subtype='html')
 
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
